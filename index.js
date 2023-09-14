a75428a2a4e325a21d5320eae3f8568efdd605f1
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-09-14 20:21:16 +0530

    Working on community share feature. Configure postRoute for creating post in cloudinary

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dallERoutes from "./routes/dallERoutes.js";
